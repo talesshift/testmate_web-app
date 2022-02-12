8df43af79119686c7bdc8365c10d5d761e38c03f
tales shift <31869464+talesshift@users.noreply.github.com>
2022-02-12 15:16:46 -0300

    added dropdown menu
    updated "Header" component form Class Component to Functional Component. and added the ability to create sub-menus in the navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './sass/App.scss'
-import { Header } from './components/header'
+import Header from './components/header'
 import logo from './images/LogoTestmate.svg';
  console.log(typeof logo);
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,33 +1,113 @@
-import React from "react";
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import React, { FunctionComponent, useState } from "react";
 
-export class Header extends React.Component<{logo: string}, {}>{
-    
-    /* const [style, setStyle] = useState({display: 'none'}); */
+type TesteProps = {
+    logo: string,
+}
 
-    render() {
-        return(
-            <nav className='navbar'>
-                <div className='navbar__container'>
-                    <div className='navbar__logo'>
-                        <img src={this.props.logo} />
+const Header: FunctionComponent<TesteProps> = ({logo}) => {
+
+ /*    const [style, setStyle] = useState({display: 'none'});
+                    <div className='navbar__page' onMouseEnter={e => {setStyle({display: 'flex'});}} onMouseLeave={e => {setStyle({display: 'none'})}}>
+                        <a className="navbar__link" href="#">platform</a>
+                        <div className='navbar__hidden' style={style}>
+                            <a className="navbar__link-hidden" href="#">pages</a>
+                            <a className="navbar__link-hidden" href="#">go in here</a>
+                            <a className="navbar__link-hidden" href="#">thanks</a>
+                        </div>
                     </div>
-                    <div className='navbar__pages'>
-                        <div className='navbar__page'><a href="#">platform</a></div>
-                        <div className='navbar__page'><a href="#">services</a></div>
-                        <div className='navbar__page'><a href="#">roles</a></div>
-                        <div className='navbar__page'><a href="#">resources</a></div>
-                        <div className='navbar__page'><a href="#">contact</a></div>
-                        <div className='navbar__page'><a href="#">abou us</a></div>
-                    </div>
-                    <div className='navbar__buttons'>
-                        <button className='navbar__button navbar__button--login' >Login</button>
-                        <button className='navbar__button navbar__button--call' >Book a free call</button>
-                    </div>
+ */
+
+    return(
+        <nav className='navbar'>
+            <div className='navbar__container'>
+                <div className='navbar__logo'>
+                    <a>
+                        <img src={logo} />
+                    </a>
                 </div>
-            </nav>
-        )
-    }
+                    <div className='navbar__pages'>
+                    
+                    <Navitem text="platform" href="#"></Navitem>
+                    <Navitem text="services" href="www.google.com">
+                        <a className="navbar__link navbar__link-hidden" href="#">pages</a>
+                        <a className="navbar__link navbar__link-hidden" href="#">go in here</a>
+                        <a className="navbar__link navbar__link-hidden" href="#">thanks</a>
+                    </Navitem>
+                    <div className='navbar__page'><a className="navbar__link" href="#">roles</a></div>
+                    <div className='navbar__page'><a className="navbar__link" href="#">resources</a></div>
+                    <div className='navbar__page'><a className="navbar__link" href="#">contact</a></div>
+                    <div className='navbar__page'><a className="navbar__link" href="#">abou us</a></div>
+                </div>
+                <div className='navbar__buttons'>
+                    <button className='navbar__button navbar__button--login' >Login</button>
+                    <button className='navbar__button navbar__button--call' >Book a free call</button>
+                </div>
+            </div>
+        </nav>
+    )
+
+};
+
+
+
+const Navitem: FunctionComponent<{ text: string, href:string }> = ({children,text}) => {
+
+    const [disp, setDisp] = useState({display: 'none'});
+    const [focus, setFocus] = useState({});
+    console.log(children);
+    return(
+        <div className='navbar__page' 
+            onMouseEnter={e => {
+                if(children!==undefined){
+                    setDisp({display: 'flex'});
+                    setFocus({color: '#545454'});
+                }
+            }} 
+            onMouseLeave={e => {
+                if(children!==undefined){
+                    setDisp({display: 'none'});
+                    setFocus({});
+                }
+            }}
+        >
+            <a className="navbar__link" style={focus} href="#">{text}</a>
+            <div className='navbar__hidden' style={disp}>
+                <SvgComponent></SvgComponent>
+                {children}
+            </div>
+        </div>
+    )
+
 }
 
 
+const SvgComponent = (props:any) => {
+  
+  return(<svg
+    width={14}
+    height={7}
+    fill="none"
+    style={{
+      position: "absolute",
+      top: "-7px",
+      left: "10px",
+      transition: "transform .5s cubic-bezier(.17,.93,.38,1) 0s",
+    }}
+    {...props}
+  >
+    <path
+      d="M7 0 0 7h14L7 0Z"
+      style={{
+        fill: "#fff",
+      }}
+    />
+    <path
+      d="M7 1.41 12.59 7H14L7 0 0 7h1.41L7 1.41Z"
+      style={{
+        fill: "rgb(196, 196, 196)",
+      }}
+    />
+  </svg>)
+}
+
+export default Header
