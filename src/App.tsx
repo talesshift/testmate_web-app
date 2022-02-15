912b0c9c1c921287fdf80f2592d1ba8d99f1a794
tales shift <31869464+talesshift@users.noreply.github.com>
2022-02-15 18:42:21 -0300

    added, first content block component prototype
    added style and html structure for the first (home) content block, and placed content.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './sass/App.scss'
 import Header from './components/header'
 import Block1 from './components/block1'
 import logo from './images/LogoTestmate.svg';
+import good_company from './images/home/good_company.png';
+
  console.log(typeof logo);
 
 //prettier (formatador auto)
@@ -12,7 +14,7 @@ function App() {
     <div>
       <Header logo={logo}/>
       <div className='flexAll'> 
-        <Block1 prop="oi"/>
+        <Block1 image={good_company}/>
       </div>
     </div>
   )
--- src/components/block1.tsx
+++ src/components/block1.tsx
@@ -1,12 +1,20 @@
 import React, { FunctionComponent, useState } from "react";
 
 type TesteProps = {
-    prop: string,
+    image: string,
 }
 
-const Block1: FunctionComponent<TesteProps> = ({prop}) => {
+const Block1: FunctionComponent<TesteProps> = ({image}) => {
     return(
-        <div className='oi'>{prop}</div>
+        <div className='Block1_container'>
+            <div className="block1_text">
+                <h1 className="block1_Title">Test your website with <span className="orange">real Australian users</span></h1>
+                <p className="block1_paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div className="block1_images">
+                <img className="block1_image" src={image}></img>
+            </div>
+        </div>
     )
 }
 
